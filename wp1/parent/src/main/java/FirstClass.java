b190f9ea17e17b4440ae342b96e14b2d8df637ad
Balaji.vijayakumar <bala111990@gmail.com>
2014-11-09 02:37:17 +0000

    my second commit

---
+++ wp1/parent/src/main/java/FirstClass.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author MY-PC
+ *
+ */
+public class FirstClass {
+
+	/**
+	 * 
+	 */
+	public FirstClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello my dear poc im nearing the goal");
+
+	}
+
+}
